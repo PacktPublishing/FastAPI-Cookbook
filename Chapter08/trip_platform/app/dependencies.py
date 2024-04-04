9cb40f4004333e238b353821ae0cf527e2cf7dc9
Giunio <giuniodl@live.it>
2024-04-04 16:26:51 +0400

    Chapter08 dependency injection recipe

---
+++ Chapter08/trip_platform/app/dependencies.py
@@ -0,0 +1,114 @@
+from datetime import date, timedelta
+from typing import Annotated, Tuple
+
+from fastapi import Depends, HTTPException, Path, Query
+
+
+def check_start_end_condition(start: date, end: date):
+    if end and end < start:
+        raise HTTPException(
+            status_code=400,
+            detail=(
+                "End date must be "
+                "greater than start date"
+            ),
+        )
+
+
+def time_range(
+    start: date | None = Query(
+        default=date.today(),
+        description=(
+            "If not provided the current date is used"
+        ),
+        example=date.today().isoformat(),
+    ),
+    end: date | None = Query(
+        None,
+        example=date.today() + timedelta(days=7),
+    ),
+) -> Tuple[date, date | None]:
+    check_start_end_condition(start, end)
+    return start, end
+
+
+def select_category(
+    category: Annotated[
+        str,
+        Path(
+            description=(
+                "Kind of travel "
+                "you are interested in"
+            ),
+            enum=[
+                "cruises",
+                "city-breaks",
+                "resort-stays",
+            ],
+        ),
+    ],
+) -> str:
+    return category
+
+
+def check_coupon_validity(
+    category: Annotated[select_category, Depends()],
+    code: str | None = Query(
+        None, description="Coupon code"
+    ),
+) -> bool:
+    coupon_dict = {
+        "cruises": "CRUISE10",
+        "city-breaks": "CITYBREAK15",
+        "resort-stays": "RESORT20",
+    }
+    if (
+        code is not None
+        and coupon_dict.get(category, ...) == code
+    ):
+        return True
+    return False
+
+
+class CommonQueryParams:
+    def __init__(
+        self,
+        category: Annotated[
+            str | None,
+            Path(
+                description=(
+                    "Kind of travel "
+                    "you are interested in"
+                ),
+                enum=[
+                    "cruises",
+                    "city-breaks",
+                    "resort-stays",
+                ],
+            ),
+        ],
+        start: Annotated[
+            date | None,
+            Query(
+                description="required if end date is not provided",
+                example="2023-01-01",
+            ),
+        ] = None,
+        end: Annotated[
+            date | None,
+            Query(
+                description="required if start date is not provided",
+                example="2023-01-01",
+            ),
+        ] = None,
+        code: str | None = Query(
+            None, description="Coupon code"
+        ),
+    ):
+        check_start_end_condition(start, end)
+        self.start = start
+        self.end = end
+        self.category = category
+        self.applicable_discount = (
+            check_coupon_validity(category, code)
+        )
---
+++ Chapter08/trip_platform/app/main.py
@@ -0,0 +1,68 @@
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+
+from app.dependencies import (
+    CommonQueryParams,
+    check_coupon_validity,
+    select_category,
+    time_range,
+)
+
+app = FastAPI()
+
+
+@app.get("/v1/trips")
+def get_trips(
+    time_range: Annotated[time_range, Depends()],
+):
+    start, end = time_range
+    message = f"Request trips from {start}"
+    if end:
+        return f"{message} to {end}"
+    return message
+
+
+@app.get("/v2/trips/{category}")
+def get_trips_by_category(
+    category: Annotated[select_category, Depends()],
+    time_range: Annotated[time_range, Depends()],
+    discount_applicable: Annotated[
+        bool, Depends(check_coupon_validity)
+    ],
+):
+    start, end = time_range
+    category = category.replace("-", " ").title()
+    message = f"You requested {category} trips wihtin"
+    message += f" from {start}"
+    if end:
+        message += f" to {end}"
+    if discount_applicable:
+        message += (
+            "\n. The coupon code is valid! "
+            "You will get a discount!"
+        )
+    return message
+
+
+@app.get("/v3/trips/{category}")
+def get_trips_by_category_v3(
+    common_params: Annotated[
+        CommonQueryParams, Depends()
+    ],
+):
+    start = common_params.start
+    end = common_params.end
+    category = common_params.category.replace(
+        "-", " "
+    ).title()
+    message = f"You requested {category} trips within"
+    message += f" from {start}"
+    if end:
+        message += f" to {end}"
+    if common_params.applicable_discount:
+        message += (
+            "\n. The coupon code is valid! "
+            "You will get a discount!"
+        )
+    return message
