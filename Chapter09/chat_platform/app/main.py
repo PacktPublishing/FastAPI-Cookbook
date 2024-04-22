5d291d1701cbf1374f25926e3010ac736d05d938
Giunio <giuniodl@live.it>
2024-04-22 13:32:04 +0400

    recipe setup websocket endpoint

--- Chapter09/chat_platform/app/main.py
+++ Chapter09/chat_platform/app/main.py
@@ -1,20 +1,12 @@
-import logging
-
 from fastapi import FastAPI, WebSocket
-from fastapi.websockets import WebSocketDisconnect
 
 app = FastAPI()
 
 
-logger = logging.getLogger("uvicorn")
-
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+@app.websocket("/chatroom")
+async def chatroom(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        logger.info(f"Message received: {data}")
-        if data == "quit":
-            break
+    await websocket.send_text(
+        "Welcome to the chat room!"
+    )
     await websocket.close()
