fb4272240f6c497751cc210cf0a0c9caf0fb7c34
Giunio <giuniodl@live.it>
2024-04-23 09:42:03 +0400

    manage websocket connections disconnections recipe chapter 9

--- Chapter09/chat_platform/app/main.py
+++ Chapter09/chat_platform/app/main.py
@@ -1,12 +1,32 @@
-from fastapi import FastAPI, WebSocket
+import logging
+
+from fastapi import (
+    FastAPI,
+    WebSocket,
+)
+from fastapi.websockets import WebSocketDisconnect
 
 app = FastAPI()
+
+logger = logging.getLogger("uvicorn")
 
 
 @app.websocket("/chatroom")
 async def chatroom(websocket: WebSocket):
+    if not websocket.headers.get("Authorization"):
+        return
     await websocket.accept()
     await websocket.send_text(
         "Welcome to the chat room!"
     )
-    await websocket.close()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            logger.info(f"Message received: {data}")
+            if data == "disconnect":
+                logger.warn("Disconnecting...")
+                await websocket.close()
+                break
+
+    except WebSocketDisconnect:
+        logger.warn("Connection closed")
