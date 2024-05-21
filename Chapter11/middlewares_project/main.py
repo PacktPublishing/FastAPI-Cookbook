3a03525307a41fdb973acab95c9b0ceeae92079f
Giunio <giuniodl@live.it>
2024-05-21 13:24:55 +0400

    add middleware request modification

--- Chapter11/middlewares_project/main.py
+++ Chapter11/middlewares_project/main.py
@@ -1,15 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, APIRouter
 from starlette.middleware import Middleware
-from asgi_middleware import (
+from middlewares.asgi_middleware import (
     ASGIMiddleware,
     asgi_decorator,
 )
+from middlewares.request_modification import (
+    HashBodyContentMiddleWare,
+)
+import logging
+
+logger = logging.getLogger("uvicorn")
 
 app = FastAPI(
-    middleware=[Middleware(asgi_decorator), Middleware(ASGIMiddleware)]
+    middleware=[
+        # Middleware(asgi_decorator),
+        # Middleware(ASGIMiddleware),
+        Middleware(
+            HashBodyContentMiddleWare,
+            allowed_paths=["/send"],
+        ),
+    ]
 )
 
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/send")
+async def send(message: str = Body()):
+    logger.info(f"Message: {message}")
+    return message
---
+++ Chapter11/middlewares_project/middlewares/request_modification.py
@@ -0,0 +1,50 @@
+import logging
+from starlette.types import (
+    ASGIApp,
+    Scope,
+    Receive,
+    Send,
+    Message,
+)
+from hashlib import sha1
+
+logger = logging.getLogger("uvicorn")
+
+
+class HashBodyContentMiddleWare:
+    def __init__(
+        self, app: ASGIApp, allowed_paths: list[str]
+    ):
+        self.app = app
+        self.allowed_paths = allowed_paths
+
+    async def __call__(
+        self,
+        scope: Scope,
+        receive: Receive,
+        send: Send,
+    ):
+        if (
+            scope["type"] != "http"
+            or scope["path"]
+            not in self.allowed_paths
+        ):
+            await self.app(scope, receive, send)
+            return
+
+        async def modify_body() -> Message:
+            message = await receive()
+            assert message["type"] == "http.request"
+
+            body = message.get("body", b"")
+            message["body"] = (
+                f'"{sha1(body).hexdigest()}"'.encode()
+            )
+
+            return message
+
+        await self.app(
+            scope,
+            modify_body,
+            send,
+        )
