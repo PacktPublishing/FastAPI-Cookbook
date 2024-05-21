c318c04fa4cd2a01158d10627ec8bde37b475bf7
Giunio <giuniodl@live.it>
2024-05-21 16:22:35 +0400

    CORS policy header middleware draft

--- Chapter11/middlewares_project/main.py
+++ Chapter11/middlewares_project/main.py
@@ -7,6 +7,11 @@ from middlewares.asgi_middleware import (
 from middlewares.request_modification import (
     HashBodyContentMiddleWare,
 )
+from starlette.middleware import Middleware
+from middlewares.response_modification import (
+    ExtraResponseHeadersMiddleware,
+)
+from fastapi.middleware.cors import CORSMiddleware
 import logging
 
 logger = logging.getLogger("uvicorn")
@@ -18,6 +23,16 @@ app = FastAPI(
         Middleware(
             HashBodyContentMiddleWare,
             allowed_paths=["/send"],
+        ),
+        Middleware(
+            ExtraResponseHeadersMiddleware,
+            headers=(
+                ("new-header", "fastapi-cookbook"),
+                (
+                    "another-header",
+                    "fastapi-cookbook",
+                ),
+            ),
         ),
     ]
 )
@@ -32,3 +47,31 @@ async def read_root():
 async def send(message: str = Body()):
     logger.info(f"Message: {message}")
     return message
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+subapp = FastAPI()
+
+subapp.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@subapp.get("/")
+async def read_root_sub():
+    return {"Hello": "World from subapp"}
+
+
+app.mount("/subapp", subapp)
