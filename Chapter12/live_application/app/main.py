593c289e810f2604b51ffa269019563b6acc31bb
Giunio <giuniodl@live.it>
2024-05-30 12:32:14 +0400

    add https middleware in comment for chapter 12 recipe

--- Chapter12/live_application/app/main.py
+++ Chapter12/live_application/app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 
+"""
+Uncomment the following lines to
+enable HTTPS redirection
+only works when the server is running with HTTPS
+"""
+
+# from fastapi.middleware.httpsredirect import (
+#     HTTPSRedirectMiddleware,
+# )
+
 app = FastAPI(title="FastAPI Live Application")
+
+# app.add_middleware(HTTPSRedirectMiddleware)
 
 
 @app.get("/")
